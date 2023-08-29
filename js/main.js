912eb92abcc5fb311bcc04fb3e2e73e464f8be2e
AndreaCoggio94 <andreacoggio94@gmail.com>
2023-08-29 17:44:24 +0200

    added image for the bonus

--- js/main.js
+++ js/main.js
@@ -1,17 +1,10 @@
 const { createApp } = Vue;
 
-let app1 = createApp({
+createApp({
   data() {
     return {
       message: "Hello there!",
-    };
-  },
-}).mount("#app");
-
-let app2 = createApp({
-  data() {
-    return {
       img: "https://vuejs.org/images/logo.png",
     };
   },
-}).mount("#image");
+}).mount("#app");
